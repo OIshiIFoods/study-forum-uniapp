f793ef195ce4ea8b907bd00e57d791ad295f75b8
LiZhen <3081085609@qq.com>
2025-01-26 10:31:42 +0800

    feat: 增加类型文件

---
+++ src/service/types/api.d.ts
@@ -0,0 +1,469 @@
+import { OrderByOption } from "@/utils/buildDBQuery"
+import { ArticleProps, FileStatusEnum, FollowProps, UserFileProps, UserProps } from "./db"
+
+/** 用户登录 */
+export namespace PostUserLogin {
+    type Request = {
+        /** 验证码 */
+        captcha: string
+        /** 手机号 */
+        phoneNumber: string
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: {
+            /** token */
+            token: string
+        }
+    }
+}
+
+/** 修改用户信息 */
+export namespace PutUserInfo {
+    type Request = {
+        /** 头像链接 */
+        avatarLink?: string;
+        /** 生日 */
+        birthday?: string;
+        /** 昵称 */
+        nickname?: string;
+        /** 性别 */
+        sex?: number;
+        /** 签名 */
+        signature?: string;
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: {
+            userInfo: UserProps
+        }
+    }
+}
+
+/** 关注用户 */
+export namespace PostFollowUser {
+    type Request = {
+        /** 关注用户id */
+        followedUserId: number
+        /** 是否关注 */
+        isFollow: boolean
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
+
+/** 获取用户关注列表 */
+export namespace GetFollowUserList {
+    type Request = {}
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: {
+            /** 用户列表 */
+            followList: (Omit<FollowProps, 'userId'> & {
+                /** 是否互关 */
+                isMutual: 0 | 1
+                /** 被关注用户昵称 */
+                followedUserAvatarLink: string
+                /** 被关注用户头像链接 */
+                followedUserAvatarLink: string
+            })[]
+        }
+    }
+}
+
+/** 上传文件 */
+export namespace PostUploadFile {
+    type Request = {
+        /** 文件 */
+        files: File[]
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: {
+            uploadFileInfoList: {
+                /** 文件保存的文件名 */
+                filename: string
+                /** 原始文件名 */
+                originalname: string
+                /** 编码方式 */
+                encoding: string
+                /** MIME 类型 */
+                mimetype: string
+                /** 文件大小（字节） */
+                size: number
+                /** 文件链接 */
+                url: string
+            }[]
+        }
+    }
+
+}
+
+/** 验证码校验 */
+export namespace PostOauthCaptcha {
+    type Request = {
+        /** 手机号 */
+        phoneNumber: string
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: {
+            /** 有效时长 */
+            effectiveDuration: number
+        }
+    }
+}
+
+/** 添加用户文件 */
+export namespace PostAddUserFile {
+    type Request = {
+        /** 父级路径 */
+        parentPath: string
+        /** 文件名称 */
+        filename: string
+        /** 原始文件名称 */
+        originalname: string
+        /** 文件类型 */
+        mimetype: string
+        /** 是否为目录 */
+        isDir: boolean
+        /** 文件大小 */
+        size: number
+        /** 访问权限[0:私有, 1公共]  */
+        accessPermissions: 0 | 1
+    }[]
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
+
+/** 复制用户文件 */
+export namespace PostCopyUserFile {
+    type Request = {
+        /** 要被复制的文件id列表 */
+        copiedFileIdList: number[]
+        /** 存储路径 */
+        savePath: string
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
+
+/** 获取文件列表 */
+export namespace GetFileList {
+    type Request = Pick<UserFileProps, 'parentPath' | 'status'>
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        /** 响应数据 */
+        data: {
+            /** 文件信息列表 */
+            fileInfoList: UserFileProps[]
+        }
+    }
+}
+
+/** 修改用户文件信息 */
+export namespace PutUserFileInfo {
+    type Request = (Pick<UserFileProps, 'id'> & Partial<Omit<UserFileProps, 'id' | 'userId' | 'createTime' | 'updateTime' | 'deleteTime' | 'isDir' | 'originFileId'>>)[]
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
+
+/** 删除用户文件 */
+export namespace DeleteUserFile {
+    type Request = {
+        /** 文件id列表 */
+        fileIdList: number[]
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
+
+/** 创建文件下载链接 */
+export namespace PostFileDownloadLink {
+    type Request = {
+        /** 文件id列表 */
+        fileIdList: number[]
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: {
+            /** 下载链接 */
+            downloadLink: string
+            /** 有效时间 */
+            effectiveDuration: number
+        }
+    }
+}
+
+/** 发布文章 */
+export namespace PostPublishArticle {
+    type Request = {
+        /** 标题 */
+        title: string
+        /** 内容 */
+        content: string
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
+
+/** 删除文章 */
+export namespace DeleteArticle {
+    type Request = {
+        /** 文章id列表 */
+        articleIdList: number[]
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
+
+/** 修改文章 */
+export namespace PutArticle {
+    type Request = {
+        /** 文章id */
+        articleId: number
+        /** 标题 */
+        title: string
+        /** 内容 */
+        content: string
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
+
+/** 获取文章详细信息 */
+export namespace GetArticleDetailInfo {
+    type Request = {
+        /** 文章id */
+        articleId: number
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: {
+            /** 文章信息 */
+            articleInfo: Omit<ArticleProps, 'userId'> & {
+                isLiked: 1 | 0
+                isCollected: 1 | 0
+            }
+            /** 用户信息 */
+            userInfo: Pick<ArticleProps, 'userId'> & Pick<UserProps, 'nickname' | 'avatarLink'>
+            /** 评论列表 */
+            commenList: (ArticleCommentProps & Pick<UserProps, 'nickname' | 'avatarLink'>)[]
+        }
+    }
+}
+
+/** 获取文章列表 */
+export namespace GetArticleList {
+    type Request = Partial<Omit<ArticleProps, 'deleteTime' | 'updateTime' | 'createTime'>> & {
+        isLiked?: 1 | 0
+        isCollected?: 1 | 0
+        limit?: number
+        deleteTime?: [string, string],
+        updateTime?: [string, string],
+        createTime?: [string, string],
+        orderBy?: OrderByOption<keyof ArticleProps>[],
+    }
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: {
+            /** 文章列表 */
+            articleList: (ArticleProps & Pick<UserProps, 'nickname' | 'avatarLink'>)[]
+        }
+    }
+}
+
+/** 点赞文章 */
+export namespace PostLikeArticle {
+    type Request = {
+        /** 文章id */
+        articleId: number
+        /** 点赞状态 */
+        isLiked: 1 | 0
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
+
+/** 收藏文章 */
+export namespace PostCollectArticle {
+    type Request = {
+        /** 文章id */
+        articleId: number
+        /** 收藏状态 */
+        isCollected: 1 | 0
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
+
+/** 评论文章 */
+export namespace PostCommentArticle {
+    type Request = {
+        /** 文章id */
+        articleId: number
+        /** 评论内容 */
+        content: string
+        /** 父级评论id */
+        parentCommentId?: number
+        /** 回复目标评论id */
+        toCommentId?: number
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
+
+/** 删除文章评论 */
+export namespace DeleteArticleComment {
+    type Request = {
+        /** 评论id */
+        commentIdList: number[]
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
+
+/** 获取文章评论列表 */
+export namespace GetArticleCommentList {
+    type Request = {
+        /** 文章id */
+        articleId?: number
+        /** 评论id */
+        commentId?: number
+        /** 父级评论的 ID */
+        parentCommentId?: number
+    }
+
+    type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: {
+            /** 评论列表 */
+            commentList: (ArticleCommentProps & Pick<UserProps, 'nickname' | 'avatarLink'>)[]
+        }
+    }
+}
+
+/** 给文章评论点赞 */
+namespace PostLikeArticleComment {
+    export type Request = {
+        /** 评论ID */
+        commentId: number
+    }
+
+    export type Response = {
+        /** 状态码 */
+        status: number,
+        /** 响应信息 */
+        message: string,
+        data: any
+    }
+}
---
+++ src/service/types/db.d.ts
@@ -0,0 +1,221 @@
+/** 空间开放状态枚举 */
+export const enum SpaceOpenStatusEnum {
+    Open = 0,
+    Close = 1
+}
+
+/** 文件状态枚举 */
+export const enum FileStatusEnum {
+    RecycleBin = 0,
+    Normal = 1
+}
+
+/** 文件访问权限枚举 */
+export const enum FileAccessPermissionsEnum {
+    Private = 0,
+    Public = 1
+}
+
+/** 执行增删改mysql语句后返回的对象 */
+export interface MysqlOkPacket {
+    /** 结果集中字段的数量 */
+    fieldCount: number;
+    /** 受SQL语句影响的行数 */
+    affectedRows: number;
+    /** 第一个插入行的自增ID */
+    insertId: number;
+    /** 服务器的状态标志 */
+    serverStatus: number;
+    /** 执行SQL语句时产生的警告数量 */
+    warningCount: number;
+    /** 关于操作的附加信息 */
+    message: string;
+    /** 是否使用了MySQL 4.1及更高版本的协议 */
+    protocol41: boolean;
+    /** 实际被更改的行数 */
+    changedRows: number;
+}
+
+/** 执行mysql语句失败后返回的对象 */
+export interface MySqlError {
+    /** 错误代码 */
+    code: string;
+    /** 错误编号 */
+    errno: number;
+    /** SQL 错误消息 */
+    sqlMessage: string;
+    /** SQL 状态码 */
+    sqlState: string;
+    /** 错误索引 */
+    index: number;
+    /** 导致错误的 SQL 语句 */
+    sql: string;
+}
+
+/** 数据库用户表字段信息 */
+export interface UserProps {
+    /** 用户id */
+    id: number,
+    /** 手机号 */
+    phoneNumber: string,
+    /** 用户昵称 */
+    nickname: string,
+    /** 注册日期 */
+    registerDate?: string,
+    /** 签名 */
+    signature: string,
+    /** 头像链接 */
+    avatarLink: string,
+    /** 性别，[0:保密,1:男, 2女] */
+    sex?: 0 | 1 | 2,
+    /** 生日 */
+    birthday?: string
+    /** 空间开放状态 */
+    space_open_status: SpaceOpenStatusEnum
+    /** 可使用空间大小 字节*/
+    availableSpaceSize: number
+    /** 已使用空间大小 字节*/
+    usedSpaceSize: number
+    /** 文章数量 */
+    articleCount: number,
+    /** 关注数量 */
+    followCount: number,
+    /** 粉丝数量 */
+    fansCount: number,
+    /** 点赞的文章数量 */
+    likeCount: number,
+    /** 收藏的文章数量 */
+    collectionCount: number
+}
+
+/** 数据库 file 表字段信息 */
+export interface FileProps {
+    /** 文件id */
+    id: number;
+    /** 保存路径 */
+    savePath: string;
+    /** 源文件名称 */
+    name: string;
+    /** 文件类型 */
+    type: string;
+    /** 文件大小 字节*/
+    size: number;
+    /** 引用数量 */
+    quoteCount: number;
+    /** 存储时间 */
+    saveTime: string;
+    /** 上传文件的用户id */
+    userId: number;
+}
+
+/** 数据库 UserFile 表字段信息 */
+export interface UserFileProps {
+    /** 用户文件id */
+    id: number;
+    /** 用户id */
+    userId: number;
+    /** 文件名称 */
+    name: string;
+    /** 文件类型 */
+    type: string;
+    /** 父级路径 */
+    parentPath: string;
+    /** 文件状态 */
+    status: FileStatusEnum;
+    /** 文件创建时间 */
+    createTime: string;
+    /** 文件更新时间 */
+    updateTime: string;
+    /** 文件删除时间（为空时代表未删除） */
+    deleteTime?: string;
+    /** 是否为目录 0否1是 */
+    isDir: 0 | 1;
+    /** 访问权限 0 私有 1公共*/
+    accessPermissions: FileAccessPermissionsEnum;
+    /** 源文件id */
+    originFileId?: number;
+}
+
+/** 数据库 Article 表字段信息 */
+export interface ArticleProps {
+    /** 文章ID */
+    id: number;
+    /** 用户ID */
+    userId: number;
+    /** 标题 */
+    title: string;
+    /** 正文内容 */
+    content: string;
+    /** 点赞数量 */
+    likeCount: number;
+    /** 评论数量 */
+    commentCount: number;
+    /** 收藏数量 */
+    collectionCount: number;
+    /** 浏览量 */
+    viewCount: number;
+    /** 创建时间 */
+    createTime: string;
+    /** 更新时间 */
+    updateTime: string;
+    /** 删除时间（为空时代表未删除） */
+    deleteTime?: string;
+}
+
+/** 数据库 article_like 表字段信息 */
+export interface ArticleLikeProps {
+    /** id */
+    id: number
+    /** 用户ID */
+    userId: number;
+    /** 文章ID */
+    articleId: number;
+    /** 点赞时间 */
+    createTime: string;
+}
+
+/** 数据库 article_collection 表字段信息 */
+export interface ArticleCollectionProps {
+    /** id */
+    id: number
+    /** 用户ID */
+    userId: number;
+    /** 文章ID */
+    articleId: number;
+    /** 点赞时间 */
+    createTime: string;
+}
+
+/** 数据库 follow 表的字段信息 */
+export interface FollowProps {
+    /** id */
+    id: number
+    /** 用户ID */
+    userId: number;
+    /** 关注用户ID */
+    followedUserId: number;
+    /** 关注时间 */
+    createTime: string;
+}
+
+/** 数据库文章评论表的属性 */
+export interface ArticleCommentProps {
+    /** 评论的唯一标识符 */
+    id: number
+    /** 评论内容 */
+    content: string
+    /** 评论创建时间 */
+    createTime: string
+    /** 评论更新时间 */
+    updateTime: string
+    /** 父级评论的 ID */
+    parentCommentId: number | null
+    /** 回复目标评论的 ID */
+    toCommentId: number | null
+    /** 发表评论的用户 ID */
+    userId: number
+    /** 文章 ID */
+    articleId: number
+    /** 点赞的用户列表 */
+    likedUserList: string
+}
