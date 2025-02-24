297973f233265076de5e6b10978b898b720f4b6f
LiZhen <3081085609@qq.com>
2025-02-24 16:51:54 +0800

    feat: 增加工具函数 filterObjectByKeys

---
+++ src/utils/index.ts
@@ -0,0 +1,14 @@
+export const filterObjectByKeys = (
+  obj: Record<string, any>,
+  keys: string[]
+): Record<string, any> => {
+  return keys.reduce(
+    (acc, key) => {
+      if (key in obj) {
+        acc[key] = obj[key]
+      }
+      return acc
+    },
+    {} as Record<string, any>
+  )
+}
