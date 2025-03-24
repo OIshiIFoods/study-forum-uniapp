908f4f32f0e726d87c268f6f154543afa5189366
LiZhen <3081085609@qq.com>
2025-03-24 18:29:09 +0800

    feat: 注册 notice socket 事件

---
+++ src/api/socket.ts
@@ -0,0 +1,30 @@
+import { useUserMessage } from '@/hooks/useUserMessage'
+import { baseURL } from './http'
+import MySocket from './modules/socket'
+import { useNotice } from '@/hooks/useNotice'
+
+const socket = new MySocket({
+  url: baseURL!.replace(/(http)|(https)/, 'ws'),
+  header: {
+    Authorization: `Bearer ${uni.getStorageSync('token')}`,
+  },
+})
+
+const { syncRemoteMessages } = useUserMessage()
+const { addNotice } = useNotice()
+
+socket.registerMessageEvent('message', async (params) => {
+  const resData = JSON.parse(params.data)
+  if (resData.type === 'syncRemoteMessages') {
+    await syncRemoteMessages()
+  }
+})
+
+socket.registerMessageEvent('notice', async (params) => {
+  const resData = JSON.parse(params.data)
+  if (resData.type === 'newNotice') {
+    addNotice(resData.data)
+  }
+})
+
+export default socket
