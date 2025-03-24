17109b8ba28c62e3bda23c84aa09a00c781e6bb2
LiZhen <3081085609@qq.com>
2025-03-24 18:18:34 +0800

    feat: 新增 useNotice hook

---
+++ src/hooks/useNotice.ts
@@ -0,0 +1,28 @@
+import { getNoticeList } from '@/service'
+import { NoticeProps } from '@/service/types/db'
+import { ref } from 'vue'
+
+const notices = ref<NoticeProps[]>([])
+
+export const useNotice = () => {
+  const init = async () => {
+    await syncRemoteNotices()
+  }
+  /** 同步远程通知信息 */
+  const syncRemoteNotices = async () => {
+    const getMsgListRes = await getNoticeList()
+    const noticeList = getMsgListRes.data.noticeList
+    notices.value = noticeList
+  }
+
+  /** 添加通知 */
+  const addNotice = (notice: NoticeProps) => {
+    notices.value.unshift(notice)
+  }
+
+  return {
+    init,
+    addNotice,
+    syncRemoteNotices,
+  }
+}
