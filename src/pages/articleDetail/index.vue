180de72ac509716493790e2fec271113940e5996
LiZhen <3081085609@qq.com>
2025-04-09 18:29:00 +0800

    feat: 跳转文章详情页时, 校验当前用户是否有权限

--- src/pages/articleDetail/index.vue
+++ src/pages/articleDetail/index.vue
@@ -1,213 +1,226 @@
 <template>
-  <view class="px-10px min-h-100vh">
-    <up-navbar :title="'坛友动态'" placeholder>
-      <template #left>
-        <view
-          class="flex items-center justify-between b-0.5px b-solid b-#efeff1 rounded-100px p-[3px_7px] opacity-80"
-        >
-          <up-icon
-            v-if="!isFirstPage"
-            name="arrow-left"
-            size="19"
-            @click="router.back()"
+  <view
+    v-if="
+      !(
+        userInfo?.userId !== userStore.id &&
+        articleInfo?.status !== ArticleStatusEnum.Public
+      )
+    "
+  >
+    <view class="px-10px min-h-100vh">
+      <up-navbar :title="'坛友动态'" placeholder>
+        <template #left>
+          <view
+            class="flex items-center justify-between b-0.5px b-solid b-#efeff1 rounded-100px p-[3px_7px] opacity-80"
+          >
+            <up-icon
+              v-if="!isFirstPage"
+              name="arrow-left"
+              size="19"
+              @click="router.back()"
+            />
+            <up-line
+              v-if="!isFirstPage"
+              direction="col"
+              :hairline="false"
+              length="16"
+              margin="0 8px"
+            />
+            <up-icon
+              name="home"
+              size="20"
+              @click="router.pushTab({ name: 'index' })"
+            />
+          </view>
+        </template>
+      </up-navbar>
+      <view class="flex items-center justify-between">
+        <view class="flex items-center">
+          <up-avatar
+            class="mx-[10px]"
+            :src="baseURL + '' + userInfo?.avatarLink"
+            :size="40"
+            @click="
+              () => {
+                clickAvatar({ userId: userInfo!.userId })
+              }
+            "
           />
-          <up-line
-            v-if="!isFirstPage"
-            direction="col"
-            :hairline="false"
-            length="16"
-            margin="0 8px"
-          />
-          <up-icon
-            name="home"
-            size="20"
-            @click="router.pushTab({ name: 'index' })"
-          />
+          <view class="text-[#333] text-[16px]">
+            {{ userInfo?.nickname }}
+          </view>
         </view>
-      </template>
-    </up-navbar>
-    <view class="flex items-center justify-between">
-      <view class="flex items-center">
-        <up-avatar
-          class="mx-[10px]"
-          :src="baseURL + '' + userInfo?.avatarLink"
-          :size="40"
+        <up-button
+          v-if="userInfo?.userId !== userStore.id"
+          :custom-style="{
+            borderColor: 'var(--primary-color)',
+            width: 'auto',
+            margin: '0',
+          }"
+          :color="'var(--primary-color)'"
+          :size="'small'"
+          :shape="'circle'"
+          :icon="isFollow ? 'checkmark' : ''"
+          :iconColor="'white'"
+          :text="isFollow ? '已关注' : '关注'"
           @click="
-            () => {
-              clickAvatar({ userId: userInfo!.userId })
+            async () => {
+              const hasFollow = isFollow
+              await followUser({
+                followedUserId: userInfo!.userId,
+                isFollow: !hasFollow,
+              })
+              await userStore.syncUserInfo()
+              mV.uni.showToast({
+                title: hasFollow ? '取消关注成功' : '关注成功',
+                icon: 'none',
+              })
             }
           "
         />
-        <view class="text-[#333] text-[16px]">
-          {{ userInfo?.nickname }}
+      </view>
+      <view class="mt-15px">
+        <view class="text-[#333] text-[20px] font-700">
+          {{ articleInfo?.title }}
         </view>
+        <up-line />
+        <view class="mt-10px">
+          <up-parse :content="articleInfo?.content" />
+        </view>
+        <view class="my-15px text-[#999] text-[12px]">
+          {{ dayjs(articleInfo?.updateTime).format('YYYY-MM-DD') }}
+        </view>
+        <up-line />
       </view>
-      <up-button
-        v-if="userInfo?.userId !== userStore.id"
-        :custom-style="{
-          borderColor: 'var(--primary-color)',
-          width: 'auto',
-          margin: '0',
-        }"
-        :color="'var(--primary-color)'"
-        :size="'small'"
-        :shape="'circle'"
-        :icon="isFollow ? 'checkmark' : ''"
-        :iconColor="'white'"
-        :text="isFollow ? '已关注' : '关注'"
-        @click="
-          async () => {
-            const hasFollow = isFollow
-            await followUser({
-              followedUserId: userInfo!.userId,
-              isFollow: !hasFollow,
-            })
-            await userStore.syncUserInfo()
-            mV.uni.showToast({
-              title: hasFollow ? '取消关注成功' : '关注成功',
-              icon: 'none',
-            })
-          }
-        "
+      <CommentSection
+        ref="commentSecRef"
+        v-model:comment-data="commentData"
+        :onLike="likeComment"
+        :onDelete="delComment"
+        :onAdd="addComment"
+        :onAvatarClick="clickAvatar"
       />
     </view>
-    <view class="mt-15px">
-      <view class="text-[#333] text-[20px] font-700">
-        {{ articleInfo?.title }}
-      </view>
-      <up-line />
-      <view class="mt-10px">
-        <up-parse :content="articleInfo?.content" />
-      </view>
-      <view class="my-15px text-[#999] text-[12px]">
-        {{ dayjs(articleInfo?.updateTime).format('YYYY-MM-DD') }}
-      </view>
-      <up-line />
-    </view>
-    <CommentSection
-      ref="commentSecRef"
-      v-model:comment-data="commentData"
-      :onLike="likeComment"
-      :onDelete="delComment"
-      :onAdd="addComment"
-      :onAvatarClick="clickAvatar"
-    />
-  </view>
-  <view class="flex sticky bottom-0 bg-white p-10px b-t b-t-solid b-t-#ebeced">
-    <input
-      class="flex-1 bg-[rgba(224,224,224,.5)] p-[6px_12px] text-14px text-[rgb(216,216,216)] rounded-full"
-      placeholder="说点什么"
-      disabled
-      @click="commentSecRef?.showCommentInputSection"
-    />
-    <view class="flex items-center gap-15px ml-20px">
-      <up-icon
-        :name="
-          articleInfo?.collectedUsers.includes(userStore.id ?? 0)
-            ? 'star-fill'
-            : 'star'
-        "
-        :color="
-          articleInfo?.collectedUsers.includes(userStore.id ?? 0)
-            ? '#FFA500'
-            : ''
-        "
-        :size="30"
-        :label="articleInfo?.collectedUsers.length ?? 0"
-        @click="
-          async () => {
-            if (!articleInfo?.id) {
-              mV.uni.showToast({
-                title: '文章不存在',
-                icon: 'none',
-              })
-              return
-            }
-            const isCollected = articleInfo?.collectedUsers.includes(
-              userStore.id ?? 0
-            )
-            await collectArticle({
-              articleId: articleInfo?.id,
-              isCollected: isCollected ? 0 : 1,
-            })
-            if (isCollected) {
-              articleInfo.collectedUsers.splice(
-                articleInfo.collectedUsers.indexOf(userStore.id!),
-                1
-              )
-            } else {
-              articleInfo.collectedUsers.push(userStore.id!)
-            }
-          }
-        "
+    <view
+      class="flex sticky bottom-0 bg-white p-10px b-t b-t-solid b-t-#ebeced"
+    >
+      <input
+        class="flex-1 bg-[rgba(224,224,224,.5)] p-[6px_12px] text-14px text-[rgb(216,216,216)] rounded-full"
+        placeholder="说点什么"
+        disabled
+        @click="commentSecRef?.showCommentInputSection"
       />
-      <up-icon
-        :name="
-          articleInfo?.likedUsers.includes(userStore.id ?? 0)
-            ? 'thumb-up-fill'
-            : 'thumb-up'
-        "
-        :color="
-          articleInfo?.likedUsers.includes(userStore.id ?? 0) ? '#59a3f4' : ''
-        "
-        :size="30"
-        :label="articleInfo?.likedUsers.length ?? 0"
-        @click="
-          async () => {
-            if (!articleInfo?.id) {
-              mV.uni.showToast({
-                title: '文章不存在',
-                icon: 'none',
-              })
-              return
-            }
-            const isLiked = articleInfo?.likedUsers.includes(userStore.id ?? 0)
-            await likeArticle({
-              articleId: articleInfo?.id,
-              isLiked: isLiked ? 0 : 1,
-            })
-            if (isLiked) {
-              articleInfo.likedUsers.splice(
-                articleInfo.likedUsers.indexOf(userStore.id!),
-                1
-              )
-            } else {
-              articleInfo.likedUsers.push(userStore.id!)
-            }
-          }
-        "
-      />
-      <view
-        class="pos-relative"
-        v-if="moreActions.find((item) => !item.hidden)"
-      >
+      <view class="flex items-center gap-15px ml-20px">
         <up-icon
-          name="more-dot-fill"
-          @click="showMoreOperation = !showMoreOperation"
+          :name="
+            articleInfo?.collectedUsers.includes(userStore.id ?? 0)
+              ? 'star-fill'
+              : 'star'
+          "
+          :color="
+            articleInfo?.collectedUsers.includes(userStore.id ?? 0)
+              ? '#FFA500'
+              : ''
+          "
+          :size="30"
+          :label="articleInfo?.collectedUsers.length ?? 0"
+          @click="
+            async () => {
+              if (!articleInfo?.id) {
+                mV.uni.showToast({
+                  title: '文章不存在',
+                  icon: 'none',
+                })
+                return
+              }
+              const isCollected = articleInfo?.collectedUsers.includes(
+                userStore.id ?? 0
+              )
+              await collectArticle({
+                articleId: articleInfo?.id,
+                isCollected: isCollected ? 0 : 1,
+              })
+              if (isCollected) {
+                articleInfo.collectedUsers.splice(
+                  articleInfo.collectedUsers.indexOf(userStore.id!),
+                  1
+                )
+              } else {
+                articleInfo.collectedUsers.push(userStore.id!)
+              }
+            }
+          "
+        />
+        <up-icon
+          :name="
+            articleInfo?.likedUsers.includes(userStore.id ?? 0)
+              ? 'thumb-up-fill'
+              : 'thumb-up'
+          "
+          :color="
+            articleInfo?.likedUsers.includes(userStore.id ?? 0) ? '#59a3f4' : ''
+          "
+          :size="30"
+          :label="articleInfo?.likedUsers.length ?? 0"
+          @click="
+            async () => {
+              if (!articleInfo?.id) {
+                mV.uni.showToast({
+                  title: '文章不存在',
+                  icon: 'none',
+                })
+                return
+              }
+              const isLiked = articleInfo?.likedUsers.includes(
+                userStore.id ?? 0
+              )
+              await likeArticle({
+                articleId: articleInfo?.id,
+                isLiked: isLiked ? 0 : 1,
+              })
+              if (isLiked) {
+                articleInfo.likedUsers.splice(
+                  articleInfo.likedUsers.indexOf(userStore.id!),
+                  1
+                )
+              } else {
+                articleInfo.likedUsers.push(userStore.id!)
+              }
+            }
+          "
         />
         <view
-          class="pos-absolute pos-bottom-[200%] pos-right-50% flex flex-col justify-between items-center transition-all bg-white overflow-hidden shadow-2xl rounded-[4px] box-border"
-          :class="[showMoreOperation ? 'h-80px p-8px' : 'h-0 p-0']"
+          class="pos-relative"
+          v-if="moreActions.find((item) => !item.hidden)"
         >
+          <up-icon
+            name="more-dot-fill"
+            @click="showMoreOperation = !showMoreOperation"
+          />
           <view
-            class="relative"
-            v-for="(item, index) in moreActions"
-            :key="index"
-            v-show="!item.hidden"
+            class="pos-absolute pos-bottom-[200%] pos-right-50% flex flex-col justify-between items-center transition-all bg-white overflow-hidden shadow-2xl rounded-[4px] box-border"
+            :class="[showMoreOperation ? 'h-80px p-8px' : 'h-0 p-0']"
           >
-            <up-icon
-              class="whitespace-nowrap"
-              :name="item.icon"
-              :label="item.label"
-              :color="item.color"
-              :labelSize="16"
-              @click="item.clickAction"
-            />
-            <button
-              class="absolute pos-top-0 w-[100%] h-[100%] bg-[rgba(0,0,0,0)] after:b-0"
-              v-if="item.name === 'share'"
-              open-type="share"
-            />
+            <view
+              class="relative"
+              v-for="(item, index) in moreActions"
+              :key="index"
+              v-show="!item.hidden"
+            >
+              <up-icon
+                class="whitespace-nowrap"
+                :name="item.icon"
+                :label="item.label"
+                :color="item.color"
+                :labelSize="16"
+                @click="item.clickAction"
+              />
+              <button
+                class="absolute pos-top-0 w-[100%] h-[100%] bg-[rgba(0,0,0,0)] after:b-0"
+                v-if="item.name === 'share'"
+                open-type="share"
+              />
+            </view>
           </view>
         </view>
       </view>
@@ -246,7 +259,11 @@ import type {
   OnLikeProps,
 } from '@/components/CommentSection/index.vue'
 import router from '@/router'
-import type { ArticleCommentProps, UserProps } from '@/service/types/db'
+import {
+  ArticleStatusEnum,
+  type ArticleCommentProps,
+  type UserProps,
+} from '@/service/types/db.d'
 
 type OnloadParamType =
   | {
@@ -272,6 +289,19 @@ onLoad(async (param) => {
     userAvatarLink: baseURL + '' + userStore.avatarLink,
     commentCount: articleData.commentList.length,
     commentList: articleData.commentList.map(formatCommentItem),
+  }
+  if (
+    userInfo.value?.userId !== userStore.id &&
+    articleInfo.value.status !== ArticleStatusEnum.Public
+  ) {
+    uni.showToast({
+      title: '无权限访问',
+      icon: 'none',
+    })
+    setTimeout(() => {
+      router.back()
+    }, 2000)
+    return
   }
 })
 
