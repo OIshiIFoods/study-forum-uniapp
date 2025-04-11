6a71218193057ef7a7ee7c3bf08513940f600eda
LiZhen <3081085609@qq.com>
2025-04-11 15:30:02 +0800

    feat: 首页和论坛页新增文章类别筛选功能

--- src/pages/forum/index.vue
+++ src/pages/forum/index.vue
@@ -14,6 +14,7 @@
         searchValue = ''
         const { data } = await getArticleList({
           ...baseSearchParams,
+          categoryId: activeArticleCatygory || undefined,
         })
         articleList = data.articleList
         mV.uni.hideLoading()
@@ -36,6 +37,7 @@
         const { data } = await getArticleList({
           title: searchValue || undefined,
           createTime: ['', articleList.at(-1)?.createTime ?? ''],
+          categoryId: activeArticleCatygory || undefined,
           ...baseSearchParams,
         })
         articleList.push(...data.articleList)
@@ -70,6 +72,7 @@
             })
             const { data } = await getArticleList({
               title: searchValue || undefined,
+              categoryId: activeArticleCatygory || undefined,
               ...baseSearchParams,
             })
             articleList = data.articleList
@@ -83,6 +86,7 @@
             })
             const { data } = await getArticleList({
               title: searchValue || undefined,
+              categoryId: activeArticleCatygory || undefined,
               ...baseSearchParams,
             })
             articleList = data.articleList
@@ -91,6 +95,21 @@
         "
         @clear="searchValue = undefined"
       />
+      <up-scroll-list :indicator="false" indicatorStyle="">
+        <view
+          v-for="(item, index) in articleCatygoryList"
+          class="text-#CCC text-13px p-[8px_10px]"
+          :class="activeArticleCatygory === item.id ? 'text-black' : ''"
+          :key="index"
+          @click="
+            () => {
+              activeArticleCatygory = item.id
+            }
+          "
+        >
+          {{ item.name }}
+        </view>
+      </up-scroll-list>
       <ArticleItem
         v-for="item in articleList"
         :articleItem="item"
@@ -107,13 +126,24 @@
 
 <script setup lang="ts">
 import ArticleItem from '@/components/ArticleItem.vue'
-import { getArticleList } from '@/service'
-import type { GetArticleList } from '@/service/types/api'
+import { getArticleCategoryList, getArticleList } from '@/service'
+import type { GetArticleCategories, GetArticleList } from '@/service/types/api'
 import { ArticleStatusEnum } from '@/service/types/db.d'
-import { onMounted, ref, watch } from 'vue'
+import { onMounted, reactive, ref, watch } from 'vue'
 
 const searchValue = ref<string | undefined>('')
+const activeArticleCatygory = ref<number>(0)
 const articleList = ref<GetArticleList.Response['data']['articleList']>([])
+const articleCatygoryList = reactive<GetArticleCategories.Response['data']>([
+  {
+    id: 0,
+    name: '全部',
+    description: '全部',
+    createdTime: '',
+    updatedTime: '',
+    deletedTime: '',
+  },
+])
 const scrollViewRelatedProps = ref({
   /** 滚动区域滚动条位置 */
   scrollTop: 0,
@@ -140,9 +170,12 @@ onMounted(async () => {
     title: '加载中',
   })
   const { data } = await getArticleList({
+    categoryId: activeArticleCatygory.value || undefined,
     ...baseSearchParams,
   })
   articleList.value = data.articleList
+  const { data: articleCatData } = await getArticleCategoryList()
+  articleCatygoryList.push(...articleCatData)
   uni.hideLoading()
 })
 
@@ -152,10 +185,23 @@ watch(searchValue, async (newVal) => {
       title: '加载中',
     })
     const { data } = await getArticleList({
+      categoryId: activeArticleCatygory.value || undefined,
       ...baseSearchParams,
     })
     articleList.value = data.articleList
     uni.hideLoading()
   }
+})
+
+watch(activeArticleCatygory, async () => {
+  uni.showLoading({
+    title: '加载中',
+  })
+  const { data: articleData } = await getArticleList({
+    ...baseSearchParams,
+    categoryId: activeArticleCatygory.value || undefined,
+  })
+  articleList.value = articleData.articleList
+  uni.hideLoading()
 })
 </script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -34,6 +34,7 @@
         const { data } = await getArticleList({
           title: searchValue || undefined,
           createTime: ['', articleList.at(-1)?.createTime ?? ''],
+          categoryId: activeArticleCatygory || undefined,
           ...baseSearchParams,
         })
         articleList.push(...data.articleList)
@@ -96,6 +97,7 @@
               })
               const { data } = await getArticleList({
                 title: searchValue || undefined,
+                categoryId: activeArticleCatygory || undefined,
                 ...baseSearchParams,
               })
               articleList = data.articleList
@@ -109,6 +111,7 @@
               })
               const { data } = await getArticleList({
                 title: searchValue || undefined,
+                categoryId: activeArticleCatygory || undefined,
                 ...baseSearchParams,
               })
               articleList = data.articleList
@@ -118,13 +121,27 @@
           @clear="searchValue = undefined"
         />
       </view>
+      <up-scroll-list :indicator="false" indicatorStyle="">
+        <view
+          v-for="(item, index) in articleCatygoryList"
+          class="text-#CCC text-13px p-[8px_10px]"
+          :class="activeArticleCatygory === item.id ? 'text-black' : ''"
+          :key="index"
+          @click="
+            () => {
+              activeArticleCatygory = item.id
+            }
+          "
+        >
+          {{ item.name }}
+        </view>
+      </up-scroll-list>
       <ArticleItem
         v-for="item in articleList"
         :articleItem="item"
         :key="item.id"
       />
       <up-loadmore
-        v-if="articleList.length > 0"
         height="50"
         icon
         :status="scrollViewRelatedProps.isLower ? 'loading' : 'nomore'"
@@ -136,14 +153,25 @@
 <script setup lang="ts">
 import ArticleItem from '@/components/ArticleItem.vue'
 import router from '@/router'
-import { getArticleList } from '@/service'
+import { getArticleCategoryList, getArticleList } from '@/service'
 import { getSwiperList } from '@/service/modules/common'
-import type { GetArticleList } from '@/service/types/api'
+import type { GetArticleCategories, GetArticleList } from '@/service/types/api'
 import { ArticleStatusEnum } from '@/service/types/db.d'
-import { nextTick, onMounted, ref, watch } from 'vue'
+import { nextTick, onMounted, reactive, ref, watch } from 'vue'
 
 const swiperList = ref<{ id: number; url: string; articleId?: number }[]>([])
 const searchValue = ref<string | undefined>('')
+const activeArticleCatygory = ref<number>(0)
+const articleCatygoryList = reactive<GetArticleCategories.Response['data']>([
+  {
+    id: 0,
+    name: '全部',
+    description: '全部',
+    createdTime: '',
+    updatedTime: '',
+    deletedTime: '',
+  },
+])
 const articleList = ref<GetArticleList.Response['data']['articleList']>([])
 const officialUSERID = +(process.env.Official_USER_ID ?? 0)
 const scrollViewRelatedProps = ref({
@@ -175,8 +203,28 @@ onMounted(async () => {
 
 watch(searchValue, async (newVal) => {
   if (newVal === '') {
-    await initalData()
+    uni.showLoading({
+      title: '加载中',
+    })
+    const { data } = await getArticleList({
+      ...baseSearchParams,
+      categoryId: activeArticleCatygory.value || undefined,
+    })
+    articleList.value = data.articleList
+    uni.hideLoading()
   }
+})
+
+watch(activeArticleCatygory, async () => {
+  uni.showLoading({
+    title: '加载中',
+  })
+  const { data: articleData } = await getArticleList({
+    ...baseSearchParams,
+    categoryId: activeArticleCatygory.value || undefined,
+  })
+  articleList.value = articleData.articleList
+  uni.hideLoading()
 })
 
 const initalData = async () => {
@@ -185,10 +233,13 @@ const initalData = async () => {
   })
   searchValue.value = ''
   swiperList.value = await getSwiperList()
-  const { data } = await getArticleList({
+  const { data: articleData } = await getArticleList({
     ...baseSearchParams,
+    categoryId: activeArticleCatygory.value || undefined,
   })
-  articleList.value = data.articleList
+  articleList.value = articleData.articleList
+  const { data: articleCatData } = await getArticleCategoryList()
+  articleCatygoryList.push(...articleCatData)
   uni.hideLoading()
 }
 
