4dc36461bf517e2c0fb07f38b345954cab780935
LiZhen <3081085609@qq.com>
2025-04-11 21:31:39 +0800

    feat: 评论区输入框使用富文本编辑框

--- src/components/CommentSection/components/CommentEditor.vue
+++ src/components/CommentSection/components/CommentEditor.vue
@@ -1,48 +1,63 @@
 <template>
   <up-popup :show="isShowPopup" :onClose="() => (isShowPopup = false)">
-    <view class="bg-white box-border p-10px">
+    <view v-if="isShowPopup" class="bg-white box-border p-10px">
       <view>
-        <textarea
+        <SvEditor
           class="rounded-8px bg-[rgba(224,224,224,.5)] text-13px w-full box-border p-10px max-h-200rpx"
-          v-model="inputContent.content"
+          pasteMode="origin"
           :placeholder="inputContent.placeholder || '留下你的想法吧'"
+          @ready="(e: any) => (editorCtx = e)"
+          @input="({ text = '', html = '' }) => (inputContent.content = text)"
         />
       </view>
-      <view class="flex justify-between my-6px">
-        <view class="flex gap-row-5px"></view>
-        <view>
-          <up-button
-            :disabled="inputContent.content.length === 0"
-            :shape="'circle'"
-            :type="'primary'"
-            :size="'small'"
-            @click="emit('send', toRaw(inputContent))"
-          >
-            发送
-          </up-button>
-        </view>
-      </view>
+      <SvEditorToolbar
+        ref="toolbarRef"
+        :tools="['emoji']"
+        :toolbarStyle="{ flex: 'none', width: 'auto' }"
+        :toolbarItemStyle="{ flex: 'none', width: '80rpx' }"
+        :activeToolStyle="{ color: 'var(--primary-color)' }"
+      >
+        <template #toolbarRight>
+          <view class="flex gap-10px mr-10px">
+            <up-button
+              :disabled="inputContent.content.length === 0"
+              :shape="'circle'"
+              :type="'primary'"
+              :size="'small'"
+              @click="emit('send', toRaw(inputContent))"
+            >
+              发送
+            </up-button>
+          </view>
+        </template>
+      </SvEditorToolbar>
     </view>
   </up-popup>
 </template>
 
 <script setup lang="ts">
 import { reactive, ref, toRaw, watch } from 'vue'
+import SvEditor from '@/plugins/sv-editor/components/sv-editor/sv-editor.vue'
+import SvEditorToolbar from '@/plugins/sv-editor/components/sv-editor/sv-editor-toolbar.vue'
 
+const emit = defineEmits(['send'])
+
+const editorCtx = ref<any>(null)
+
+const isShowPopup = ref(false)
 const inputContent = reactive({
   placeholder: '',
   content: '',
 })
-
-const emit = defineEmits(['send'])
-
-const isShowPopup = ref(false)
 
 /** 当弹窗关闭时重置信息 */
 watch(
   () => isShowPopup.value,
   (val) => {
     if (!val) {
+      editorCtx.value.setContents({
+        html: '',
+      })
       inputContent.content = ''
       inputContent.placeholder = ''
     }
